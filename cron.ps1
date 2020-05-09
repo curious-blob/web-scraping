3ea0d3887646d2f12d57541cedab7d1f3d140f66
unknown <omarkhantlp@gmail.com>
2020-05-09 04:28:36 +0300

    init

---
+++ cron.ps1
@@ -0,0 +1,5 @@
+﻿$action = "python3 cron.py"
+$trigger = New-JobTrigger -Once -At 9:00AM -RepetitionInterval (New-TimeSpan -Hours 12) -RepeatIndefinitely
+$options = New-ScheduledJobOption -RunElevated -RequireNetwork
+
+Register-ScheduledJob -Name Scrape-Dataset -Trigger $trigger -ScriptBlock $action -MaxResultCount 5 -ScheduledJobOption $options
