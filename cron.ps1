4aa50a179ff0a54b65d7acbc715c1e9c41c548d5
Omar <omarkhantlp@gmail.com>
2020-05-14 05:35:51 +0300

    updated dependency

--- cron.ps1
+++
@@ -1,5 +0,0 @@
-﻿$action = "python3 cron.py"
-$trigger = New-JobTrigger -Once -At 9:00AM -RepetitionInterval (New-TimeSpan -Hours 12) -RepeatIndefinitely
-$options = New-ScheduledJobOption -RunElevated -RequireNetwork
-
-Register-ScheduledJob -Name Scrape-Dataset -Trigger $trigger -ScriptBlock $action -MaxResultCount 5 -ScheduledJobOption $options
